eac4b1755bd8bdba785252c8c6f9effe740eac9d
Sage N Clements <66354152+SageKnowsIT@users.noreply.github.com>
2024-12-09 09:26:42 -0600

    Add files via upload
    PLEASE NOTE: A rate limiter has not been implemented on this script.  Please use responsibly.
    
    Python script requires the follow package(s):
    
    csv
    requests

---
+++ bulkLinkChecker.py
@@ -0,0 +1,18 @@
+import requests
+
+
+
+# Objective - Take CVS File and check the requests status.  Output the link into three categories: Existing (Valid), Not Existing (404), Redirect.  Additionally, check to see if each link has a sub-domain of www or /
+with open('test.csv','r')
+
+# Open CSV File as Read only
+
+
+# For Loop
+
+
+# Print Status to console.  Keep Wait
+
+
+
+#Export - Do this last
